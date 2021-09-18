f27db4f1d25fc2c234588b61bcfa5efa43645dda
drinith <felpmel@gmail.com>
2021-09-18 19:02:41 -0300

    respondendo a API

--- src/Context.js
+++ src/Context.js
@@ -2,10 +2,10 @@ import React, { createContext, useEffect, useState } from 'react'
 import axios from 'axios';//Axios é um cliente HTTP baseado em Promises para fazer requisições.
 import keys from './config/keys'
 
-export const myContext = createContext({name:"default",cargo:'default'})
+export const myContext = createContext()
 //Buscar o usuário que está na API se existir e jogar no contexto
 const Context = (props) => {
-    const [userObject, setUserObject] = useState({name:"default",cargo:'default'});
+    const [userObject, setUserObject] = useState();
     const [cargo,setCargo]=useState();
 
     useEffect(() => {
@@ -13,13 +13,13 @@ const Context = (props) => {
         axios.get('http://'+keys.serverApi + "/getuser", { withCredentials: true }).then((res) => {
             console.log(res.data);
             if (res.data) {
-                setUserObject(res.data);
+                setUserObject(res.data.name);
             }
         })
     }, [])
 
     return (
-        <myContext.Provider value={userObject.name}>{props.children}</myContext.Provider>
+        <myContext.Provider value={userObject}>{props.children}</myContext.Provider>
     )
 }
 export default Context;
